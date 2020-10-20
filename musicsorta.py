520d82450ed72c4fd01564547ea9cdb6501705d6
SpiffyB <tom.macarthur@bath.edu>
2020-10-20 18:58:35 +0100

    checks file name

--- musicsorta.py
+++ musicsorta.py
@@ -1,8 +1,12 @@
 import re
 import argparse
 import os
+import pprint
+
+FILE_EXTENSIONS = (".mp3", ".wav", ".m4a", ".flac")
 
 dir_struct = {}
+pass_list = []
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Music library sorter')
@@ -18,10 +22,16 @@ def main():
     args = parse_args()
 
     for filename in os.listdir(args.library):
+        if not filename.endswith(FILE_EXTENSIONS):
+            continue
+
         match = re.match(pattern, filename)
-        if match.group(1) not in dir_struct:
-            dir_struct[match.group(1)] = {}
-        dir_struct[match.group(1)][match.group(2)] = filename
+        if match is None:
+            pass_list.append(filename)
+        else:
+            if match.group(1) not in dir_struct:
+                dir_struct[match.group(1)] = {}
+            dir_struct[match.group(1)][match.group(2)] = filename
 
 if __name__ == "__main__":
     main()
